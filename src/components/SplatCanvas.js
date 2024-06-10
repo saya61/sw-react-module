bfc23d28592464601199b727159532eb99a798d0
msaki <msaki061@gmail.com>
2024-06-10 20:32:08 +0900

    module 2

--- src/components/SplatCanvas.js
+++ src/components/SplatCanvas.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
-import { cameras, getViewMatrix, getProjectionMatrix } from './main';
+import {getViewMatrix, getProjectionMatrix, cameras} from './main';
 
 function SplatCanvas() {
     const canvasRef = useRef(null);
